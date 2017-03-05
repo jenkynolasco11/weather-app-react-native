cb9700906c3631157078a9c747ed226a009c2bcd
Jenky W. Nolasco <jenky.nolasco@gmail.com>
2017-03-05 17:56:06 -0400

    added fetching temp from server

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  KEY : 'react-native-secret',
+  //
+}
--- index.android.js
+++ index.android.js
@@ -1,53 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
+  View,
   Text,
-  View
-} from 'react-native';
+  StyleSheet
+} from 'react-native'
+import config from './config'
 
-export default class WeatherAppReactNative extends Component {
+const styles = StyleSheet.create({
+  main : {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : 'steelblue'
+  },
+  text : {
+    fontSize : 20,
+    fontWeight : 'bold',
+    color : '#eee',
+    textAlign : 'center'
+  }
+})
+
+export default class weatherApp extends Component{
+  state = {
+    message : 'Hi, how are you doing?',
+    // initialPosition : 'unknown',
+    location : 'unknown',
+    temp : '',
+    watchID : 0,
+    tries : 1
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+      <View style={styles.main}>
+        <Text style={styles.text}>
+          {
+            `Current Position: ${ this.state.location }\n`
+          }
+          {
+            `Current Temperature: ${ this.state.temp }\n`
+          }
+          {
+            `Watch ID: ${ this.state.watchID }`
+          }
         </Text>
       </View>
-    );
+    )
+  }
+
+  componentDidMount() {
+    // navigator.geolocation.getCurrentgetCurrentPosition(
+    //   (position) => {
+    //     const initialPosition = JSON.stringify(position)
+    //     this.setState({ initialPosition })
+    //   }, (err) => {
+    //     alert(JSON.stringify(err))
+    //   }, {
+    //     enableHighAccuracy : true,
+    //     timeout : 20000,
+    //     maximumAge : 10000
+    //   })
+    const fetchTemp = async (position) => {
+      const tries = 1
+      const location = {
+        lon : position.coords.longitude,
+        lat : position.coords.latitude
+      }
+      const request = `https://rn-weather-app.herokuapp.com/${ config.KEY }/${ location.lon }/${ location.lat }`
+
+      try{
+        const data = await fetch(request)
+        const temp = JSON.stringify(await data.json())
+        this.setState({
+          temp,
+          tries,
+          location : JSON.stringify(location)
+        })
+
+      } catch(err) {
+        alert(`something happened, error: ${ err }`)
+      }
+    }
+
+    const watchID = navigator.geolocation.watchPosition(
+      fetchTemp
+      /*pos => {
+        const position = JSON.stringify({
+          longitude : pos.coords.longitude,
+          latitude : pos.coords.latitude
+        })
+
+        this.setState({ position })
+      }*/, error => {
+        alert(JSON.stringify(error))
+      }, {
+        enableHighAccuracy : true,
+        timeout : 20000,
+        maximumAge : 1000
+      })
+
+      this.setState({ watchID })
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('WeatherAppReactNative', () => WeatherAppReactNative);
+AppRegistry.registerComponent('WeatherAppReactNative', () => weatherApp)
